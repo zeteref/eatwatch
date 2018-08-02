b973938903e7d7c3a13354bcf8f2e60b8e2ddd66
zrf <zrf@z.pl>
2018-08-02 09:53:48 +0200

    trying diffrent statement locations

--- tests/add_tests.py
+++ tests/add_tests.py
@@ -20,5 +20,10 @@ class Test(unittest.TestCase):
 
 
     def test_add_ingredient(self):
-        test = add_ingredient(ingredient('kurczak', calories='100'))
+        test = add_ingredient(ingredient('test', 
+                                         calories='1',
+                                         sugar='2',
+                                         veg_protein=3,
+                                         protein=4,
+                                         carbo='5'))
         print(test)
